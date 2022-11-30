952031301cf868980600dd334649cb78c51c51e4
sukyungdev <sukyung.developer@gmail.com>
2022-11-30 18:08:34 +0900

    Add : _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+// import Header from '../component/header';
+// import Footer from '../component/footer';
+// import Layout from '../component/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* <Header /> */}
+      <Component {...pageProps} />
+      {/* <Footer /> */}
+    </div>
+  );
 }

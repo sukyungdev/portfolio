d237c8695b2525f07541c7cc3c596632edb8dd60
sukyungdev <sukyung.developer@gmail.com>
2022-12-10 23:36:49 +0900

    Design : Header.tsx ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,37 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import DarkModeButton from './DarkModeButton';
 
 export default function Header() {
   return (
     <div>
-      <header className="text-gray-600 body-font">
+      <header className="shadow">
         <div className="container mx-auto p-5 flex flex-row items-center">
           <div className="flex flex-grow">
-            <Link href={'/'} className="font-medium text-gray-900 mb-0 text-lg sm:text-xl">
-              Sukyungdev
+            <Link href={'/'} className="m-0 px-2">
+              {/* <Image src="/logo.png" alt="logo" /> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 text-yellow-400 hover:text-sky-400"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </Link>
           </div>
-          <nav className="md:ml-auto flex items-center text-sm sm:text-base justify-center ">
-            <Link href="/" className="mr-5 hover:text-gray-900">
+          <nav className="flex items-center text-sm sm:text-base justify-center ">
+            <Link href="/" className="mr-5">
               Home
             </Link>
-            <Link href="/aboutMe" className="mr-5 hover:text-gray-900">
+            <Link href="/aboutMe" className="mr-5">
               AboutMe
             </Link>
-            <Link href="/" className='className="mr-5 hover:text-gray-900"'>
+            <Link href="/" className="mr-5">
               이력서
             </Link>
           </nav>
